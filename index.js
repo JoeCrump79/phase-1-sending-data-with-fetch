498d78933aa6683fd73c2b925dc3a32a5cfefe15
Joseph Crump <josephcrump79@yahoo.com>
2024-09-14 14:47:48 -0500

    phase-1-sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+function submitData(username, email){
+    const formData = {
+        name : username,
+        email: email, 
+      };
+      
+      // method: "POST" is missing from the object below
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (object) {
+            console.log(object)
+            const id = object.id
+            const node = document.createElement("p");
+            node.append(id);
+            document.body.append(node)
+            //element.append()
+        })
+        .catch(function (object) {
+            console.log(object)
+            const msg = object.message
+            const node = document.createElement("p");
+            node.append(msg);
+            document.body.append(node)
+            //element.append()
+        })
+    }
